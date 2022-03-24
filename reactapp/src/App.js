1d4750b9d5df5ccdad0fc01869bca52eab87fa53
soumik sarkar <52997931+saikatsatkar502@users.noreply.github.com>
2022-03-24 17:25:06 +0530

    Add files via upload

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,11 +1,26 @@
-
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import AddUser from './components/AddStudent';
 import AdminStudent from './components/AdminStudent';
+import DisplayUser from './components/DisplayUser';
+import EditStudent from './components/EditStudent';
+import ViewStudent from './components/ViewStudent';
+
 
 function App() {
   return (
     <div className="App">
-      <AdminStudent/>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<AdminStudent/>} />
+          <Route exact path='/admin/viewStudent' element={<DisplayUser />} ></Route>
+          <Route exact path='/admin/addStudent' element={<AddUser/>}  />
+          <Route exact path='/admin/editStudent/:id' element={<EditStudent />} />
+          <Route exact path='/admin/viewDetailsStudent/:id' element={<ViewStudent />} />
+        </Routes>
+      </BrowserRouter>
+
     </div>
   );
 }
