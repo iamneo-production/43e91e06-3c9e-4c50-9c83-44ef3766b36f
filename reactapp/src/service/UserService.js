af7b326a194505c86614672f86055b808e7b10e3
soumik sarkar <52997931+saikatsatkar502@users.noreply.github.com>
2022-03-24 17:35:51 +0530

    Add files via upload

--- reactapp/src/service/UserService.js
+++ reactapp/src/service/UserService.js
@@ -1,10 +1,25 @@
 import axios from "axios";
 import api from '../api/User';
+class UserSetvice{
 
-class UserService{
-    getStudent(){
-        return api.get();
+    getStudents() {
+        return api.get("/Students")
     }
+    getByStudentId(id) {
+        console.log(id);
+        return api.get(`/Students/${id}`);
+    }
+
+    addStudent(student){
+        return api.post("/Students",student);
+    }
+    editStudent(student,id){
+        return api.put(`/Students/${id}`,student);
+    }
+    deleteStudent(id){
+        return axios.delete("http://localhost:3006/Students/"+id);
+    }
+
 }
 
-export default new UserService();
+export default new UserSetvice();
