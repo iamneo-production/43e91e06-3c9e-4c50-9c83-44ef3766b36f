73c8190d03905a5a686c8a0dea516eb8dd9f52f8
saikatsarkar502 <saikatsarkar502@gmail.com>
2022-03-31 16:04:28 +0000

    Login and spring app added

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,3 +1,30 @@
+import React from 'react';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Dashboard from './components/Dashboard';
+import User from './components/User';
+export default function App() {
+  return (
+    
+      <BrowserRouter>
+      <div >
+        <Routes>
+        <Route exact path="/" element={<Login></Login>}/>
+          <Route exact  path="/Signup" element={<Signup></Signup>}/>
+          <Route exact path="/Admin/Dashboard" element={<Dashboard/>}/>
+          <Route exact path="/User/Dashboard" element={<User/>}/>
+        </Routes>
+          
+
+      </div>  
+      </BrowserRouter>
+    
+  )
+}
+
+
+
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import AddUser from './components/AddStudent';
 import AdminStudent from './components/AdminStudent';
