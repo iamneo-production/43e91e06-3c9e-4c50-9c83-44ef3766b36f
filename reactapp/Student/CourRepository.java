c23e6d9674b17f6dc1c4e55cc94b258f9a29ead5
Tulasi-Chimata <chtulasi484@gmail.com>
2022-04-18 17:01:28 +0530

    tulasi commit

---
+++ reactapp/Student/CourRepository.java
@@ -0,0 +1,16 @@
+package com.example.sprint4.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.sprint4.model.Course;
+
+
+
+public interface CourRepository extends CrudRepository<Course, Integer>{
+
+	
+	
+
+}
